2135a96fc45641b3624e63cb0d60d8b95376616a
Thabish <kadertabish2@gmail.com>
2022-12-15 08:03:17 +0400

    understnading_how_to_add_in_the_head
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- linked_list.py
+++ linked_list.py
@@ -6,12 +6,20 @@ class Node:
 class SLinkedList:
     def __init__(self) -> None:
         self.headval =None
-
+    # print linked list
     def listprint(self):
       printval = self.headval
       while printval is not None:
          print (printval.dataval)
          printval = printval.nextval
+    
+    def AtBeginning(self,newdata):
+        NewNode = Node(newdata)
+    
+    # update the new nodes next val to exisiting
+        NewNode.nextval = self.headval
+        self.headval = NewNode
+    
 
 list1 = SLinkedList()
 list1.headval = Node("Mon")
@@ -20,4 +28,6 @@ e3 = Node("Wed")
 list1.headval.nextval = e2
 e2.nextval = e3
 
+
+list1.AtBeginning("Sun")
 list1.listprint()
