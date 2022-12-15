fe0664cf77d5ab10a3f269f5139a453e9b89a32a
Thabish <kadertabish2@gmail.com>
2022-12-15 07:22:03 +0400

    solution2
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- Top_100_liked_question.py
+++ Top_100_liked_question.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, ListNode,Optional
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -11,3 +11,22 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if(nums[i] + nums[j] == target):
                     return [i,j]
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        l1_reverse= l1[::-1]
+        l2_reverse= l2[::-1]
+
+
+        s=[str(i) for i in l1_reverse]
+        s2 = [str(i) for i in l2_reverse]
+    
+        # Join list items using join()
+        res = int("".join(s))
+        res2 = int("".join(s2))
+
+        target = res + res2
+    
+    l1= [1,2,3]
+    l2=[2,3,4]
+    print(addTwoNumbers(l1,l2))
