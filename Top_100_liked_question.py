df80b77b8c21d315887a56fbf3bcf94620cdd110
Thabish <kadertabish2@gmail.com>
2022-12-27 06:25:04 +0400

    sol13-v3
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- Top_100_liked_question.py
+++ Top_100_liked_question.py
@@ -291,24 +291,24 @@ sol = Solution12()
 # print(sol.intToRoman(1994))
 
 class Solution13:
-    charToNum = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-    }
     def romanToInt(self, s:str) : 
-        prev = 0 
+        charToNum = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
+        curr = 0
+        prev = 0
         total = 0
-        for numeral in s:
-            cur = self.charToNum[numeral]
-            total += cur if cur>=prev else -1*cur
-            print(self.charToNum[numeral],total)
-            prev = cur
-        print (total)
+        for i in s[::-1]:
+            curr = charToNum[i]
+            total +=curr if curr>prev else -1*curr
+            prev = curr
+        print(total)
 
 
 sol13 = Solution13()
